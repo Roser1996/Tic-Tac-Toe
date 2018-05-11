65d561b251556be130a1ca22ced8f43bc80f0df7
Rui <rgan1@uci.edu>
2018-05-10 23:13:43 -0700

    Initial Commit

---
+++ SFML tutorial/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  SFML tutorial
+//
+//  Created by 甘睿 on 5/10/18.
+//  Copyright © 2018 Roser. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
